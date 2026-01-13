5a3534b0cf94b31cf229d2e7eb3c5bfbd538081b
Ruyter Fontes <ruyterfontes@MacBook-Pro-de-Ruyter.local>
2026-01-13 12:02:40 -0300

    Feature: Google Ads tag AW-17690718525

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -202,6 +202,19 @@ export default function RootLayout({
                         }}
                     />
                 )}
+
+                {/* Google Ads (gtag.js) */}
+                <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17690718525" />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'AW-17690718525');
+                        `
+                    }}
+                />
             </head>
             <body>
                 {/* Google Tag Manager (noscript) */}

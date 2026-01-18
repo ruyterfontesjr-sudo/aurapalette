059fc1bb1fe891c363d05ec8e32598ba45e23e4e
Ruyter Fontes <ruyterfontes@MacBook-Pro-de-Ruyter.local>
2026-01-18 19:44:14 -0300

    feat: transparent favicon with v3 cache buster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,9 +66,9 @@ export const metadata: Metadata = {
         statusBarStyle: "default",
     },
     icons: {
-        icon: '/icon.png?v=2',
-        shortcut: '/icon.png?v=2',
-        apple: '/icon.png?v=2',
+        icon: '/icon.png?v=3',
+        shortcut: '/icon.png?v=3',
+        apple: '/icon.png?v=3',
     },
 };
 
